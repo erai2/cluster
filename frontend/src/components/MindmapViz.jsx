aabc869473d790aa855cfbd32bb18317665ceb92
erai2 <gjjiy1169@gmail.com>
2025-08-13 16:40:15 +0900

    Delete frontend directory

--- frontend/src/components/MindmapViz.jsx
+++
@@ -1,7 +0,0 @@
-export default function MindmapViz({ rules }) {
-  return (
-    <div className="text-gray-600">
-      Mindmap visualization is not implemented yet.
-    </div>
-  );
-}
--- frontend/src/components/NetworkViz.jsx
+++
@@ -1,7 +0,0 @@
-export default function NetworkViz({ rules }) {
-  return (
-    <div className="text-gray-600">
-      Network visualization is not implemented yet.
-    </div>
-  );
-}
