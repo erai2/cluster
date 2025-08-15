0f34b8e5bf96469f11a598293f0a19a1e7cbd552
erai2 <gjjiy1169@gmail.com>
2025-08-15 13:20:39 +0900

    Update condition_filter.py

--- stream_/condition_filter.py
+++ stream_/condition_filter.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-import re
-from typing import Iterable, List
+from typing import Iterable
 
-KEYWORDS = ["면","허투","보면","경우","없으면","되면","강하면","약하면",
+KEYWORDS = ["면","경우","허투","보면","없으면","되면","강하면","약하면",
             "제압","穿","破","沖","合","墓","庫"]
-
-def extract_condition_candidates(text: str) -> List[str]:
-    text = re.sub(r'\n+', ' ', text)
-    parts = re.split(r'(?<=[다요음함\.!\?])\s+', text)
-    return [s.strip() for s in parts if any(k in s for k in KEYWORDS)]
 
 def filter_stream(sent_iter: Iterable[str]) -> Iterable[str]:
     for s in sent_iter:
